66bfce34649e2d552c9a82dec8852687cd476552
aymensellaouti <aymen.sellaouti@gmail.com>
2025-11-25 15:27:50 +0100

    fix bug

--- src/app/components/first/first.component.ts
+++ src/app/components/first/first.component.ts
@@ -1,14 +1,10 @@
-<<<<<<< HEAD
-import { Component, inject } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { UserInfoComponent } from '../user-info/user-info.component';
-import { Router } from '@angular/router';
-=======
-import { Component, signal } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { UserInfoComponent } from "../user-info/user-info.component";
+import { Component, inject, signal } from "@angular/core";
+import { FormsModule } from "@angular/forms";
+import { Router } from "@angular/router";
 import { HighlightDirective } from "../../directives/highlight.directive";
->>>>>>> 340c86d8606b2e7d67c5d401bbb410c4fe9329fe
+import { UserInfoComponent } from "../user-info/user-info.component";
+
+
 
 @Component({
   selector: 'app-first',
@@ -20,13 +16,10 @@ export class FirstComponent {
   // State
   backgroundColor = 'yellow';
   openColor = 'yellow';
-<<<<<<< HEAD
 
   router = inject(Router);
-=======
   color = 'yellow';
   isOn = signal(false);
->>>>>>> 340c86d8606b2e7d67c5d401bbb410c4fe9329fe
   constructor() {
     // setInterval(() => {
     // }, 1500)
@@ -34,13 +27,10 @@ export class FirstComponent {
   showMessage(message: string) {
     alert(`message de mon fils: ${message}`);
   }
-<<<<<<< HEAD
+
   checkUser() {
     this.router.navigate(['user']);
   }
-=======
->>>>>>> 340c86d8606b2e7d67c5d401bbb410c4fe9329fe
-
   interrupteur() {
     this.isOn.update((value) => !value)
   }
